892812dae8c5fabb476c70bfdb17bdb60f86cee0
Thomasbsgr <thomasbsgr@gmail.com>
2026-07-06 22:10:15 +0200

    feat(i18n): generate metadata from Metadata translations

---
+++ src/lib/utils/age.ts
@@ -0,0 +1,16 @@
+export function getAge() {
+  const birthDate = new Date(2005, 8, 30);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  const hasHadBirthdayThisYear =
+    today.getMonth() > birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+  if (!hasHadBirthdayThisYear) {
+    age--;
+  }
+
+  return age;
+}
