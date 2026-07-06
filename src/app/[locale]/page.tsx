255f0706c46a1ed071aae3ee0be2daae7dbc2d1a
Thomasbsgr <thomasbsgr@gmail.com>
2026-07-06 21:43:26 +0200

    feat(i18n): add next-intl with en/fr messages, routing and middleware

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import toggleTheme from "@/lib/theme/toggleTheme";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("HomePage");
+
   return (
     <>
-      <h1>Test</h1>
+      <h1>{t("title")}</h1>
       <button onClick={() => toggleTheme()}>ici</button>
     </>
   );
